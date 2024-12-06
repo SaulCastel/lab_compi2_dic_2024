be153ef7e61bf4c897bc84107a53af21c152d2c7
Saul Castellanos <saulcastel018@gmail.com>
2024-12-05 18:27:50 -0600

    Github pages a través de github actions

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'node:fs';
-import * as Parser from './grammar.js';
-
-const filePath = process.argv[2];
-const data = readFileSync(filePath, { encoding: 'utf-8' });
-const output = Parser.parse(data.trim());
-
-console.log(output);
